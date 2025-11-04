bd2361e059948bc6464c2931c1f1b5351225d282
Preston Quinn <155991579+Quinn0324@users.noreply.github.com>
2025-11-04 13:31:42 -0500

    Everything working

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar.jsx';
+
+function App() {
+ 
+ return (
+  <><div className="font-sans">
+     <Navbar />
+   </div><div className="App">
+       <h1 className="text-3xl font-bold text-blue-500">Tailwind Works!</h1>
+     </div></>
+  );
+}
+
+export default App;
