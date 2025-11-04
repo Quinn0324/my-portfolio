05ff7fb580c04a2b798dd32a050d453c7d664197
Preston Quinn <155991579+Quinn0324@users.noreply.github.com>
2025-11-04 14:28:28 -0500

    Favicon/pagetitle

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.jsx';
+import Hero from './components/hero.jsx';
 
 function App() {
  
  return (
   <><div className="font-sans">
      <Navbar />
+     <Hero />
    </div><div className="App">
-       <h1 className="text-3xl font-bold text-blue-500">Tailwind Works!</h1>
+       <h1 className="text-3xl font-bold text-blue-900">Tailwind Works!</h1>
      </div></>
   );
 }
