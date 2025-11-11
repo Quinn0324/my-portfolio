e6bf901c7bc16a847afcd604d84b2f3e8c3f41cd
Preston Quinn <155991579+Quinn0324@users.noreply.github.com>
2025-11-11 16:09:54 -0500

    Added Style Guide Page

---
+++ my-portfolio/src/pages/style-guide.jsx
@@ -0,0 +1,9 @@
+export default function Style_Guide() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-heading mb-6">Style Guide</h1>
+      <p className="text-lg">Here’s my Style Guide</p>
+      {/* Later: Add project cards or links here */}
+    </div>
+  );
+}
